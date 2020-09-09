1c4b3e681418d6ed9bf371807689ab82f67578a3
EC2 Default User <ec2-user@ip-172-31-22-18.us-east-2.compute.internal>
2020-09-09 03:04:02 +0000

    update

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,40 +3,40 @@
 # Part 1
 
 def sum arr
-	sums=0
-	if arr.length > 0
-		arr.each do |ints|
-			sums+=ints
-		end
-	end
-	return sums
+  sums=0
+  if arr.length > 0
+    arr.each do |ints|
+    sums+=ints
+  end
+  end
+  return sums
 end
 
 def max_2_sum arr
   max1=0
   max2=0
   orders=0
-	if arr.length > 0
-		arr.each do |ints|
-		  if orders==0
+    if arr.length > 0
+	  arr.each do |ints|
+		if orders==0
+		  max1=ints
+		elsif orders==1
+		  if ints>max1
+		    max2=max1
 		    max1=ints
-		  elsif orders==1
-		    if ints>max1
-		      max2=max1
-		      max1=ints
-		    else
-		      max2=ints
-		    end
-		  elsif ints> max1
-			  max2=max1
-			  max1=ints
-		  elsif ints> max2
-			  max2=ints
+		  else
+		    max2=ints
 		  end
-		orders=orders+1
+		elsif ints> max1
+		  max2=max1
+	      max1=ints
+		elsif ints> max2
+		  max2=ints
 		end
-	end
-	return max1+max2
+		orders=orders+1
+      end
+    end
+  return max1+max2
 end
 
 def sum_to_n? arr, n
@@ -61,39 +61,37 @@ def hello(name)
 end
 
 def starts_with_consonant? s
-  if 	/^[^aeiouAEIOU\W]/i.match(s) == nil
- 		return false
+  if /^[^aeiouAEIOU\W]/i.match(s) == nil
+  	return false
   else
- 		return true
+  	return true
   end
 end
 
 def binary_multiple_of_4? s
   if  /^[01]*(00)$/.match(s)
-  	return true
+    return true
   elsif  /^(0)$/.match(s)
-  	return true
+    return true
   else
- 		return false
+    return false
   end
 end
 # Part 3
 
 class BookInStock
-		
-	attr_accessor :isbn
-	attr_accessor :price
-	
+  attr_accessor :isbn
+  attr_accessor :price
+
   def initialize isbn,price
-		@isbn=isbn
-		@price=price
-		if @isbn.empty? || @price <= 0
-			raise ArgumentError
-		end
+  	@isbn=isbn
+  	@price=price
+  	if @isbn.empty? || @price <= 0
+  	  raise ArgumentError
+  	end
   end
 
-
-	def price_as_string
-		return "$#{'%.2f' %  @price}"
-	end
+  def price_as_string
+    return "$#{'%.2f' %  @price}"
+  end
 end
